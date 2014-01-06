fb7334d6af74e63e6484083d54b9484e88548b04
ebizexpand <anil.iitk@outlook.com>
2014-01-06 22:41:00 +0530

    commited

--- src/com/ebizx/number/NumberUtils.java
+++ src/com/ebizx/number/NumberUtils.java
@@ -1,13 +1,38 @@
 package com.ebizx.number;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NumberUtils {
+
+	public static void test() {
+		Pattern p = Pattern.compile("-?\\d+");
+		Matcher m = p.matcher("There are more than -2 and less than 12 numbers here");
+		while (m.find()) {
+			System.out.println(m.group());
+		}
+	}
+
+	public static List<Integer>extractNumbersFromString(String input) {
+		List<Integer>numbers = new ArrayList<Integer>();
+		Pattern p = Pattern.compile("-?\\d+");
+		Matcher m = p.matcher(input);
+		while (m.find()) {
+			numbers.add(Integer.parseInt(m.group()));
+		}
+		return numbers;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer>numbers = extractNumbersFromString("my name1223 is why this-23 is 22 fail");
+		for (Integer num : numbers) {
+			System.out.println(num);
+		}
 	}
 
 }
